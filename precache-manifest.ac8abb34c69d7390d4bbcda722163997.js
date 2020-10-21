212dd2b48be9081799b0499701b6a59feefe710e
weitanglau <lauweitang@gmail.com>
2020-10-21 16:34:44 +0800

    Updates

---
+++ precache-manifest.ac8abb34c69d7390d4bbcda722163997.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4945810e5afc254dabacbfacad6bf593",
+    "url": "/all-storage-fe/index.html"
+  },
+  {
+    "revision": "474e197b476ed6b9619d",
+    "url": "/all-storage-fe/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "047005463187f64d6004",
+    "url": "/all-storage-fe/static/css/main.25acc211.chunk.css"
+  },
+  {
+    "revision": "474e197b476ed6b9619d",
+    "url": "/all-storage-fe/static/js/2.f8f08892.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/all-storage-fe/static/js/2.f8f08892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047005463187f64d6004",
+    "url": "/all-storage-fe/static/js/main.fffbb20c.chunk.js"
+  },
+  {
+    "revision": "1034b6bd5d9beb2e3952",
+    "url": "/all-storage-fe/static/js/runtime-main.beb08d2a.js"
+  }
+]);
